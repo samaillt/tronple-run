51792385671c355b3ee236621cbb0ed27ee8a010
BRAVO NOELIE <nbravo@localhost.localdomain>
2018-12-20 12:07:31 +0100

    doxygen Object.hpp in progress

--- tronple_run/src/project_classes/include/project_classes/Object.hpp
+++ tronple_run/src/project_classes/include/project_classes/Object.hpp
@@ -1,20 +1,50 @@
 #pragma once
 #include <iostream>
 
+/// \class Object
+/// \brief Class that defines an object
 class Object {
 
 private:
-    char _type;
-    float _width;
-    float _height;
-    float _posX;
-    float _posY;
-    float _posZ;
+    char _type; /*!<  type that defines the object
+    /// \li 'p' : player
+    /// \li 'e' : enemy
+    /// \li 'b' : block
+    /// \li 'a' : arche
+    /// \li 'c' : coin
+    /// \li 't' : bonus
+    /// \li 'm' : malus
+    /// \li 'g' : ground
+    /// \li 'h' : hole
+    /// \li 'f' : arrival*/ 
+    float _width; /*!< object width */ 
+    float _height; /*!< object height */ 
+    float _posX; /*!< object X position */ 
+    float _posY; /*!< object Y position */ 
+    float _posZ; /*!< object Z position */ 
 
 public:
 
-    // constructor
+    //// \brief Constructor
+    /// \param type : type to define the object
+    /// \li 'p' : player
+    /// \li 'e' : enemy
+    /// \li 'b' : block
+    /// \li 'a' : arche
+    /// \li 'c' : coin
+    /// \li 't' : bonus
+    /// \li 'm' : malus
+    /// \li 'g' : ground
+    /// \li 'h' : hole
+    /// \li 'f' : arrival
+    /// \param width : width of the requested object
+    /// \param height : height of the requested object
+    /// \param posX : X position of the object
+    /// \param posY : Y position of the object
+    /// \param posZ : Z position of the object
     Object(char type, float width, float height, float posX, float posY, float posZ);
+    
+    /// \brief Default constructor
     Object();
 
     // getter
